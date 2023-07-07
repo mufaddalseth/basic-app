0e1586b3652b641521f852bc381332ac73e6bd75
mufaddalseth <132192617+mufaddalseth@users.noreply.github.com>
2023-07-07 15:03:23 +0530

    third commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn learning_section.wsgi:application --bind 0.0.0.0:$PORT
--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-d54j+j85ccvp1cujum2&0++l7=7j9fzx7_-syslw*t3(yi1mlk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app' , '.now.sh']
+# ALLOWED_HOSTS = ['127.0.0.1','.vercel.app' , '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1']
+
 
 
 # Application definition
@@ -140,5 +142,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build' , 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build' , 'static')
